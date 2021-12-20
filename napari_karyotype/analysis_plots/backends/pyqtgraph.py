96bea74d89ad2ae12e389b975e434a22b9f4e312
Arne Ludwig <arne.ludwig@posteo.de>
2021-12-20 14:14:27 +0100

    Refactoring of backend `pyqtgraph`

--- napari_karyotype/analysis_plots/backends/pyqtgraph.py
+++ napari_karyotype/analysis_plots/backends/pyqtgraph.py
@@ -3,47 +3,103 @@ import pyqtgraph as pg
 from pyqtgraph.Qt import QtWidgets, mkQApp, QtGui, QtCore
 
 
-def do_plot(estimates, scaffoldSizes, matching, **kwargs):
+def do_plot(estimates, scaffoldSizes, initialMatching, **kwargs):
     # TODO show tooltip on hover with scaff/estimate name/index, size diff, abs. sizes, ...
+
     app = mkQApp("Chromosome size estimation: analysis plots")
-    main_window = MainWindow(estimates, scaffoldSizes, matching)
+
+    # Switch default order to Row-major (in accordance with numpy)
+    # Enable anti-aliasing for beautiful plots
+    pg.setConfigOptions(antialias=True, imageAxisOrder="row-major")
+
+    main_window = MainWindow(estimates, scaffoldSizes, initialMatching)
     app.exec_()
 
 
 class MainWindow(QtWidgets.QMainWindow):
-    """example application main window"""
+    """Main window for analysis plots."""
 
-    def __init__(self, estimates, scaffoldSizes, matching, *qtArgs, **qtKwargs):
-        super(MainWindow, self).__init__(*qtArgs, **qtKwargs)
+    def __init__(
+        self,
+        estimates,
+        scaffoldSizes,
+        initialMatching,
+        *qtArgs,
+        colorMap="magma",
+        **qtKwargs
+    ):
+        super().__init__(*qtArgs, **qtKwargs)
 
+        # store data that should be displayed
         self.estimates = estimates
         self.scaffoldSizes = scaffoldSizes
-        self.matching = matching
-
-        gridWidget = pg.GraphicsLayoutWidget(show=True)
-        self.setCentralWidget(gridWidget)
-        self.setWindowTitle("Chromosome size estimation: analysis plots")
-        self.resize(600, 500)
-        self.show()
-
-        correlationMatrix = 1 + np.abs(
+        # make a copy of the data and swap X and Y axis for plotting
+        self.matching = np.fliplr(initialMatching)
+        # compute abs. difference as a correlationmeasure
+        self.correlationMatrix = 1 + np.abs(
             np.atleast_2d(self.estimates).T - self.scaffoldSizes
         )
-        n, m = correlationMatrix.shape
-        # columns = ["A", "B", "C"]
+        # store matrix dimensions for concise access
+        self.n, self.m = self.correlationMatrix.shape
 
-        pg.setConfigOption(
-            "imageAxisOrder", "row-major"
-        )  # Switch default order to Row-major
-        pg.setConfigOptions(antialias=True)
+        self.colorMap = pg.colormap.get(colorMap)
 
-        correlationMatrixItem = CorrelationMatrixItem()
-        # create transform to center the corner element on the origin, for any assigned image:
+        # setup window appearance
+        self.setWindowTitle("Chromosome size estimation: analysis plots")
+        self.resize(600, 500)
+        self.setCentralWidget(pg.GraphicsLayoutWidget(show=True))
+
+        self._attachMatrixPlot()
+
+        self.show()
+
+    def _attachMatrixPlot(self):
+        self.matrixPlotItem = self.centralWidget().addPlot()
+        # orient y axis to run top-to-bottom
+        self.matrixPlotItem.invertY(True)
+        # remove data padding
+        self.matrixPlotItem.setDefaultPadding(0.0)
+        # show full frame, label tick marks at top and left sides, with some extra space for labels
+        self.matrixPlotItem.showAxes(
+            True, showValues=(True, True, False, False), size=20
+        )
+        # define major tick marks and labels:
+        # ticks = [(idx, label) for idx, label in enumerate(columns)]
+        # for side in ("left", "top", "right", "bottom"):
+        #     self.matrixPlotItem.getAxis(side).setTicks(
+        #         (ticks, [])
+        #     )  # add list of major ticks; no minor ticks
+        # include some additional space at bottom of figure
+        # self.matrixPlotItem.getAxis("bottom").setHeight(10)
+
+        # set locked aspect ratio of 1
+        centralViewBox = self.matrixPlotItem.getViewBox()
+        centralViewBox.setAspectLocked()
+
+        self._attachCorrelationMatrixItem()
+
+    def _attachCorrelationMatrixItem(self):
+        # prepare transform to center the corner element on the origin, for any assigned image
         alignPixelTransform = QtGui.QTransform().translate(-0.5, -0.5)
-        correlationMatrixItem.setTransform(alignPixelTransform)
-        correlationMatrixItem.setImage(correlationMatrix)
 
-        matchingPlotItem = pg.ScatterPlotItem(
+        self.correlationMatrixItem = CorrelationMatrixItem()
+        self.correlationMatrixItem.setTransform(alignPixelTransform)
+        self.correlationMatrixItem.setImage(self.correlationMatrix)
+
+        # display plot
+        self.matrixPlotItem.addItem(self.correlationMatrixItem)
+        # set parent widget for displaying information
+        self.correlationMatrixItem.setParent(self.matrixPlotItem)
+
+        # generate an adjustabled color bar, initially spanning min to max data value
+        bar = pg.ColorBarItem(
+            values=(np.min(self.correlationMatrix), np.max(self.correlationMatrix)),
+            colorMap=self.colorMap,
+        )
+        bar.setImageItem(self.correlationMatrixItem, insert_in=self.matrixPlotItem)
+
+        # create overlayed scatter plot for matching
+        self.matchingPlotItem = pg.ScatterPlotItem(
             size=10,
             pen=pg.mkPen(None),
             brush=pg.mkBrush(255, 255, 255, 120),
@@ -51,46 +107,10 @@ class MainWindow(QtWidgets.QMainWindow):
             hoverPen=pg.mkPen("r", width=2),
             hoverBrush=pg.mkBrush(None),
         )
-        # spots = [{"pos": (match[1], match[0]), "data": 1} for match in matching]
-        matchingPlotItem.addPoints(
-            pos=np.fliplr(self.matching), data=list(range(len(self.matching)))
-        )
-        # matchingPlotItem.sigClicked.connect(clicked)
+        matchingIndices = list(range(len(self.matching)))
+        self.matchingPlotItem.addPoints(pos=self.matching, data=matchingIndices)
 
-        plotItem = gridWidget.addPlot()  # add PlotItem to the main GraphicsLayoutWidget
-        plotItem.invertY(True)  # orient y axis to run top-to-bottom
-        plotItem.setDefaultPadding(0.0)  # plot without padding data range
-        plotItem.addItem(correlationMatrixItem)  # display correlationMatrixItem
-        correlationMatrixItem.setParent(plotItem)
-        # display correlationMatrixItem
-        plotItem.addItem(matchingPlotItem)
-        centralViewBox = plotItem.getViewBox()
-        centralViewBox.setAspectLocked()
-        # centralViewBox.setXRange(-0.5, min(n, m) - 0.5)
-        # centralViewBox.setYRange(-0.5, min(n, m) - 0.5)
-
-        # show full frame, label tick marks at top and left sides, with some extra space for labels:
-        plotItem.showAxes(True, showValues=(True, True, False, False), size=20)
-
-        # define major tick marks and labels:
-        # ticks = [(idx, label) for idx, label in enumerate(columns)]
-        # for side in ("left", "top", "right", "bottom"):
-        #     plotItem.getAxis(side).setTicks(
-        #         (ticks, [])
-        #     )  # add list of major ticks; no minor ticks
-        plotItem.getAxis("bottom").setHeight(
-            10
-        )  # include some additional space at bottom of figure
-
-        colorMap = pg.colormap.get("magma")
-        # generate an adjustabled color bar, initially spanning -1 to 1:
-        bar = pg.ColorBarItem(
-            values=(np.min(correlationMatrix), np.max(correlationMatrix)),
-            colorMap=colorMap,
-        )
-        # bar.setLogMode(y=True)
-        # link color bar and color map to correlationMatrixItem, and show it in plotItem:
-        bar.setImageItem(correlationMatrixItem, insert_in=plotItem)
+        self.matrixPlotItem.addItem(self.matchingPlotItem)
 
     def keyReleaseEvent(self, e):
         if e.key() == QtCore.Qt.Key.Key_Q:
