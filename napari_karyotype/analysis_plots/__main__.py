21672d346d87f6a0e0a3e533d035dcd9b58a799e
Arne Ludwig <arne.ludwig@posteo.de>
2021-12-22 09:55:29 +0100

    restructured package `analysis_plots`

--- napari_karyotype/analysis_plots/__main__.py
+++ napari_karyotype/analysis_plots/__main__.py
@@ -8,88 +8,21 @@ from pulp import GLPK, LpMaximize, LpMinimize, LpProblem, LpStatus, LpVariable
 import numpy as np
 import pandas as pd
 from importlib import import_module
+from napari_karyotype.analysis_plots import *
+import logging
 
-from logging import basicConfig, getLogger
-
-log = getLogger(__name__)
-
-
-module_fqn = "napari_karyotype.analysis_plots"
-
-
-def find_optimal_assignment(estimates, scaffs, *, unmatched_penalty=2.0):
-    estimates = pd.Series(estimates).values
-    scaffs = pd.Series(scaffs).values
-
-    n, m = len(estimates), len(scaffs)
-    # Absolute value of difference between each pair of estimate and scaffold
-    # estimates = np.log(estimates) / np.log(unmatched_penalty)
-    # scaffs = np.log(scaffs) / np.log(unmatched_penalty)
-    abs_diffs = np.log(np.abs(estimates.reshape(-1, 1) - scaffs))
-    model = LpProblem(name="estimate-matching", sense=LpMinimize)
-    xs = np.array(
-        [
-            [
-                LpVariable(
-                    name=f"match_{i:02d}_{j:02d}", lowBound=0, upBound=1, cat="Integer"
-                )
-                for j in range(m)
-            ]
-            for i in range(n)
-        ],
-        dtype=np.object_,
-    )
-    # Minimum abs. difference per scaffold
-    mu = np.amin(abs_diffs, axis=0)
-    assert mu.shape == (m,)
-    model += np.sum(xs * abs_diffs) + np.sum((1 + mu) * (1 - np.sum(xs, axis=0)))
-
-    for i in range(n):
-        model += (np.sum(xs[i, :]) <= 1, f"max_matches_per_estimate_{i:02d}")
-
-    for j in range(m):
-        model += (np.sum(xs[:, j]) <= 1, f"at_most_one_match_per_scaffold_{j:02d}")
-
-    if model.solve():
-        return np.array(
-            [(i, j) for j in range(m) for i in range(n) if xs[i, j].value() > 0],
-            dtype=np.int_,
-        )
-
-    return np.zeros((0, 2), dtype=np.int_)
-
-
-def analysis_plots(
-    scaffold_sizes, estimates, *, max_deviation=30e6, plotlib="pyqtgraph"
-):
-    scaffold_sizes = pd.Series(scaffold_sizes)
-    scaffold_sizes.sort_values(ascending=False, inplace=True)
-    estimates = pd.Series(estimates)
-    estimates.sort_values(ascending=False, inplace=True)
-
-    # matching = []
-    matching = find_optimal_assignment(estimates, scaffold_sizes, unmatched_penalty=2.0)
-
-    if "." in plotlib:
-        raise ValueError("plotlib must not contain dots ('.')")
-
-    try:
-        plot_backend = import_module(f".backends.{plotlib}", package=module_fqn)
-    except ModuleNotFoundError:
-        raise ValueError(f"Unrecognized plotlib: {plotlib}")
-
-    plot_backend.do_plot(estimates, scaffold_sizes, matching)
+log = logging.getLogger(__name__)
 
 
 def main():
     args = _parse_args()
 
     if args.scaffold_sizes.name.endswith(".fai"):
-        scaffold_sizes = _read_fasta_index(args.scaffold_sizes)
+        scaffold_sizes = read_fasta_index(args.scaffold_sizes)
     else:
-        scaffold_sizes = _read_tsv_data(args.scaffold_sizes, name="scaffold_sizes")
+        scaffold_sizes = read_tsv_data(args.scaffold_sizes, name="scaffold_sizes")
 
-    estimates = _read_tsv_data(args.estimates, "chromosome_estimates")
+    estimates = read_tsv_data(args.estimates, "chromosome_estimates")
 
     analysis_plots(
         scaffold_sizes,
@@ -104,7 +37,7 @@ def _parse_args(args=sys.argv[1:]):
 
     prog = sys.argv[0]
     if prog == "__main__":
-        prog = "python -m {module_fqn}"
+        prog = "python -m napari_karyotype.analysis_plots"
 
     parser = argparse.ArgumentParser(
         prog="analysis_plots",
@@ -141,46 +74,6 @@ def _parse_args(args=sys.argv[1:]):
     return parser.parse_args(args)
 
 
-def _read_fasta_index(fai_file):
-    fai = pd.read_table(
-        fai_file,
-        header=None,
-        index_col=0,
-        names=("scaffold", "size"),
-        usecols=(0, 1),
-    )
-    fai = fai.loc[:, "size"]
-    fai.name = "scaffold_sizes"
-
-    return fai
-
-
-def _read_tsv_data(tsv_file, name=None):
-    tsv = pd.read_table(tsv_file, header=None)
-
-    if tsv.shape[1] == 1:
-        # no names provided
-        tsv = tsv.loc[:, 0]
-        tsv.name = name
-    elif tsv.shape[1] >= 2:
-        if tsv.shape[1] > 2:
-            log.warning(f"ignoring additional columns in {tsv_file.name}")
-
-        # names in first column, sizes in second column
-        tsv = pd.Series(
-            tsv.iloc[:, 1].values,
-            index=tsv.iloc[:, 0],
-            name=name,
-        )
-    else:
-        raise Exception(f"empty file: {tsv_file.name}")
-
-    return tsv
-
-
 if __name__ == "__main__":
-    from logging import basicConfig
-
-    basicConfig()
-
+    logging.basicConfig()
     main()
