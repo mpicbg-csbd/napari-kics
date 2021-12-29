fixed correlation formula and automatic matching

napari_karyotype/analysis_plots/__init__.py
@@ -9,7 +9,11 @@ log = getLogger(__name__)
 
 
 def size_correlation(estimates, scaffs):
-    return np.log(1 + np.abs(estimates - scaffs, dtype=np.float_))
+    return np.exp(np.abs(np.log(estimates) - np.log(scaffs), dtype=np.float_))
+
+
+def get_initial_bounds(correlation_matrix):
+    return (1, 4)
 
 
 def find_optimal_assignment(estimates, scaffs, *, unmatched_penalty=2.0):
@@ -35,11 +39,9 @@ def find_optimal_assignment(estimates, scaffs, *, unmatched_penalty=2.0):
         dtype=np.object_,
     )
     # Minimum abs. difference per scaffold
-    mu = np.amin(correlation_matrix, axis=0)
+    mu = unmatched_penalty * 1 / np.amin(correlation_matrix, axis=0)
     assert mu.shape == (m,)
-    model += np.sum(xs * correlation_matrix) + np.sum(
-        (1 + mu) * (1 - np.sum(xs, axis=0))
-    )
+    model += np.sum(xs * correlation_matrix) + np.sum(mu * (1 - np.sum(xs, axis=0)))
 
     for i in range(n):
         model += (np.sum(xs[i, :]) <= 1, f"max_matches_per_estimate_{i:02d}")

napari_karyotype/analysis_plots/backends/pyqtgraph.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtWidgets, mkQApp, QtGui, QtCore
-from .. import size_correlation
+from .. import size_correlation, get_initial_bounds
 
 
 def do_plot(estimates, scaffoldSizes, initialMatching, **kwargs):
@@ -324,10 +324,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def _populateColorBarItem(self):
         # generate an adjustabled color bar, initially spanning min to max data value
         self.colorBarItem = pg.ColorBarItem(
-            values=(
-                np.min(self.correlationMatrix.values),
-                np.max(self.correlationMatrix.values),
-            ),
+            values=get_initial_bounds(self.correlationMatrix),
             colorMap=self.colorMap,
             label="Size correlation",
         )
@@ -468,7 +465,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.assignedChromosomesMask = self.joinedScaffoldLengths >= 1
 
         self.correlationPerChromosome = size_correlation(
-            self.estimates.values, self.joinedScaffoldLengths
+            self.estimates.values, 1 + self.joinedScaffoldLengths
         )
         self.correlationPerChromosome[~self.assignedChromosomesMask] = np.inf
         self.totalCorrelation = np.mean(