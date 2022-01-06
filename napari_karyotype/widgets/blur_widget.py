ce8c2f5dcf7640625121a5036e41faf206839795
Arne Ludwig <arne.ludwig@posteo.de>
2022-01-06 14:32:07 +0100

    fixed blur step for RGBA images

--- napari_karyotype/widgets/blur_widget.py
+++ napari_karyotype/widgets/blur_widget.py
@@ -3,7 +3,6 @@ from qtpy.QtCore import Qt
 
 
 class BlurWidget(QVBoxLayout):
-
     def __init__(self, viewer):
 
         super().__init__()
@@ -14,22 +13,32 @@ class BlurWidget(QVBoxLayout):
         # actual function
         def blur(input_image, sigma=1.0):
 
-            from skimage.color import rgb2gray
-            if (len(input_image.shape) == 3 and input_image.shape[-1] == 3):
+            from skimage.color import rgba2rgb, rgb2gray
+
+            if len(input_image.shape) == 3 and input_image.shape[-1] == 4:
+                img = rgb2gray(rgba2rgb(input_image))
+            if len(input_image.shape) == 3 and input_image.shape[-1] == 3:
                 img = rgb2gray(input_image)
-            elif (len(input_image.shape) == 3 and input_image.shape[-1] == 1) or (len(input_image.shape) == 2):
+            elif (len(input_image.shape) == 3 and input_image.shape[-1] == 1) or (
+                len(input_image.shape) == 2
+            ):
                 img = input_image
             else:
-                raise Exception(f"Cannot process image with type f{input_image.dtype} and shape {input_image.shape}.")
+                raise Exception(
+                    f"Cannot process image with type f{input_image.dtype} and shape {input_image.shape}."
+                )
 
             from skimage.filters import gaussian
+
             return gaussian(img, sigma)
 
         def blur_wrapper(sigma):
 
             if self.input_image is None:
                 if self.viewer.layers.selection.active is None:
-                    raise Exception("No available images found. Please import a karyotype first.")
+                    raise Exception(
+                        "No available images found. Please import a karyotype first."
+                    )
 
                 self.input_image = self.viewer.layers.selection.active.data
 
@@ -43,7 +52,8 @@ class BlurWidget(QVBoxLayout):
 
         # blur step description label
         blur_descr_label = QLabel(
-            "1. Select an appropriate sigma value to denoise the image with a Gaussian blur:")
+            "1. Select an appropriate sigma value to denoise the image with a Gaussian blur:"
+        )
 
         # blur slider label
         blur_sl_label = QLabel("sigma:")
@@ -60,8 +70,12 @@ class BlurWidget(QVBoxLayout):
 
         # sigma slider value
         sigma_sl_val = QLabel(f"{sigma_slider.value() / 20:0.2f}")
-        sigma_slider.valueChanged.connect(lambda e: sigma_sl_val.setText(f"{sigma_slider.value() / 20:0.2f}"))
-        sigma_slider.valueChanged.connect(lambda e: blur_wrapper(sigma_slider.value() / 20))
+        sigma_slider.valueChanged.connect(
+            lambda e: sigma_sl_val.setText(f"{sigma_slider.value() / 20:0.2f}")
+        )
+        sigma_slider.valueChanged.connect(
+            lambda e: blur_wrapper(sigma_slider.value() / 20)
+        )
 
         # threshold box
         blur_box_ = QHBoxLayout()
